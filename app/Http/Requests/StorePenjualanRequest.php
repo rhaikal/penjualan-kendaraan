0603a2287013e3fbd829477ed9c4f014c4ff6c3b
rhaikal <rhaikal101@gmail.com>
2023-02-05 23:14:18 +0700

    menambahkan rules array pada beberapa field pada store penjualan

--- app/Http/Requests/StorePenjualanRequest.php
+++ app/Http/Requests/StorePenjualanRequest.php
@@ -28,6 +28,7 @@ class StorePenjualanRequest extends FormRequest
             'metode' => ['required', Rule::in(['Cash', 'Kredit'])],
             'booking_fee' => ['required', 'integer'],
             'dp' => ['required', 'integer', 'min:1', 'max:99'],
+            'kendaraan' => ['required', 'array'],
             'kendaraan.id' => ['required', 'exists:kendaraans,_id'],
             'kendaraan.atas_nama' => ['required', 'string'],
             'kendaraan.otr' => ['required', Rule::in(['Off The Road', 'On The Road'])],
@@ -50,8 +51,10 @@ class StorePenjualanRequest extends FormRequest
 
         if($this->input('metode') == 'Kredit') {
             $rules['provisi'] = ['required', 'decimal:2'];
+            $rules['asuransi'] = ['required', 'array'];
             $rules['asuransi.jenis'] = ['required', Rule::in(['all risk', 'total loss only'])];
             $rules['asuransi.persentase'] = ['required', 'decimal:2'];
+            $rules['angsuran'] = ['required', 'array'];
             $rules['angsuran.jenis'] = ['required', Rule::in(['ADDM', 'ADDB'])];
             $rules['angsuran.jangka_waktu'] = ['required', 'integer', 'max:72']; // in month
             $rules['pembeli.berkas.kk'] = ['required', 'boolean'];
